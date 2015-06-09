64583b7e16db3debb4ad4b44c5a8bf49588cf3f9
Jody Clements <jody@neomorphic.co.uk>
2015-06-09 13:57:06 -0400

    Adds basic get method
    This can be used to get data from the individual repos of a server.

--- spec/dvid.spec.js
+++ spec/dvid.spec.js
@@ -9,17 +9,27 @@ describe('dvid', function() {
     expect(this.dvid.config.port).toBe(undefined);
   });
 
-  it("should have default values if connect is called without options", function() {
+  it("config should be undefined if connect is called without options", function() {
     this.dvid.connect();
-    expect(this.dvid.config.host).toBe('localhost');
-    expect(this.dvid.config.port).toBe(80);
+    expect(this.dvid.config.host).toBe(undefined);
+    expect(this.dvid.config.port).toBe(undefined);
   });
 
-  it("connects to a server and fetches the info", function() {
+  it("connects to a server and fetches the server info", function() {
     this.dvid.connect({host: 'emdata1', port: 8500});
+    expect(this.dvid.config.host).toBe('emdata1');
+    expect(this.dvid.config.port).toBe(8500);
     this.dvid.serverInfo(function(res){
       expect(res.Cores).toEqual('32');
     });
   });
+
+  it("connects to a server and fetches repo info", function() {
+    this.dvid.connect({host: 'emdata1', port: 8500});
+    this.dvid.get('36645473972544e39c6ed90c4643c8a9', 'info', {}, function(res){
+      expect(res.Alias).toEqual('MB-6');
+    });
+  });
+
 
 });
