06cbcf92278b0085335d28b135d4b6d163b806f9
Jody Clements <jody@neomorphic.co.uk>
2015-06-10 09:46:54 -0400

    Fixed typos and figure out how to run async tests
    In order to run tests that have an asynchronous component, the done()
    method must be passed in to the test, so that it call be called after
    all asynchronous tasks have been completed.

--- spec/dvid.spec.js
+++ spec/dvid.spec.js
@@ -9,25 +9,28 @@ describe('dvid', function() {
     expect(this.dvid.config.port).toBe(undefined);
   });
 
-  it("config should be undefined if connect is called without options", function() {
+  it("config defaults should be used if connect is called without options", function() {
     this.dvid.connect();
     expect(this.dvid.config.host).toBe(undefined);
-    expect(this.dvid.config.port).toBe(undefined);
+    expect(this.dvid.config.port).toBe(80);
   });
 
-  it("connects to a server and fetches the server info", function() {
+  it("connects to a server and fetches the server info", function(done) {
     this.dvid.connect({host: 'emdata1', port: 8500});
     expect(this.dvid.config.host).toBe('emdata1');
     expect(this.dvid.config.port).toBe(8500);
+
     this.dvid.serverInfo(function(res){
-      expect(res.Cores).toEqual('32');
+      expect(res.Cores).toBe('32');
+      done();
     });
   });
 
-  it("connects to a server and fetches repo info", function() {
+  it("connects to a server and fetches info for a single repo", function(done) {
     this.dvid.connect({host: 'emdata1', port: 8500});
     this.dvid.get('36645473972544e39c6ed90c4643c8a9', 'info', {}, function(res){
       expect(res.Alias).toEqual('MB-6');
+      done();
     });
   });
 
