d4fb108b20e9f7a7c8992afe6989bc3520c2ec29
Jody Clements <jody@neomorphic.co.uk>
2016-03-01 13:28:33 -0500

    Adds new methods and better validation
    There is now a version method on the dvid object that will return the
    current version of the javascript api. In addition, some validation on
    the options passed to the connection creation method has been added to
    make sure that there is always a hostname specified.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,41 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+
+var dvid = require('../lib/dvid');
+var Connection = require('../lib/connection');
+var pjson = require('../package.json');
+
+describe('dvid', function () {
+  it('should describe itself', function() {
+    assert.equal('This is the dvid api', dvid.about());
+  });
+  it('should return a connection object', function () {
+    var new_connection = dvid.connect({host: 'foo'});
+    assert.ok(new_connection instanceof Connection);
+  });
+  it('should report the correct version number', function() {
+    assert.equal(pjson.version, dvid.version());
+  });
+});
+
+describe('connection', function() {
+  it('should accept a configuration on creation', function() {
+    var conn = dvid.connect({host: 'http://www.example.com', port: '1234'});
+    assert.equal('1234', conn.config.port);
+    assert.equal('http://www.example.com', conn.config.host);
+  });
+
+  it('should set a default port on creation', function() {
+    var conn = dvid.connect({host: 'foo'});
+    assert.equal('80', conn.config.port);
+    assert.equal('foo', conn.config.host);
+  });
+
+  it('should throw an error with bad configuration values', function() {
+    try {
+      var conn = dvid.connect();
+    } catch (e) {
+      assert.ok(e instanceof Error);
+      assert.equal("A 'host' value must be provided to the connection on creation.", e.message);
+    }
   });
 });
