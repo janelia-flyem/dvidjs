0d4d4d726264665cb4d1453a7e869e2cbbd42abd
Jody Clements <jody@neomorphic.co.uk>
2022-07-20 14:21:09 -0400

    Creates new version of API using axios & Promises

---
+++ src/index.js
@@ -0,0 +1,137 @@
+const axios = require("axios").default;
+const url = require("url");
+
+class DVID {
+  constructor(options) {
+    options = options || {};
+    var match = /(https?):\/\/(.*)/.exec(options.host);
+    if (match) {
+      options.host = match[2];
+      options.protocol = match[1];
+    }
+    this.config = {
+      host: options.host,
+      port: options.port,
+      protocol: options.protocol || "http",
+      basename: options.basename || null,
+      username: options.username || "anonymous",
+      application: options.application || "dvidjs",
+    };
+
+    this.about = "This is the dvid api";
+  }
+
+  version() {
+    return require(__dirname + "/../package.json").version;
+  }
+
+  hostString() {
+    let hostString = "/";
+
+    if (this.config.host) {
+      hostString = this.config.protocol + "://" + this.config.host;
+    }
+
+    if (this.config.port) {
+      hostString += ":" + this.config.port;
+    }
+    if (this.config.basename) {
+      hostString += `/${this.config.basename}/`;
+    }
+    return hostString;
+  }
+
+  createUrl(path) {
+    var hostString = this.hostString();
+
+    // create the basic url from the hostname and the path string
+    var url_string = url.resolve(hostString, path.replace(/^\//,''));
+
+    // convert the string into an object so we can modify it.
+    const url_obj = url.parse(url_string, true);
+
+    // add the username and application to the query parameters
+    url_obj.query["u"] = this.config.username;
+    url_obj.query["app"] = this.config.application;
+
+    // send back the final url as a string
+    return url.format(url_obj);
+  }
+
+  async serverInfo() {
+    const fullUrl = this.createUrl("api/server/info");
+    const { data } = await axios.get(fullUrl);
+    return data;
+  }
+
+  async serverTypes() {
+    const fullUrl = this.createUrl("api/server/types");
+    const { data } = await axios.get(fullUrl);
+    return data;
+  }
+
+  async serverCompiledTypes() {
+    const fullUrl = this.createUrl("api/server/compiled-types");
+    const { data } = await axios.get(fullUrl);
+    return data;
+  }
+
+  async load() {
+    const fullUrl = this.createUrl("api/load");
+    const { data } = await axios.get(fullUrl);
+    return data;
+  }
+
+  async reposInfo() {
+    const fullUrl = this.createUrl("api/repos/info");
+    const { data } = await axios.get(fullUrl);
+    return data;
+  }
+
+  async repo(options) {
+    options = options || {};
+    if (!Object.hasOwnProperty.call(options, 'uuid')) {
+      throw new Error('UUID required to access repo data');
+    }
+    if (!Object.hasOwnProperty.call(options, 'endpoint')) {
+      throw new Error('endpoint required to access repo data');
+    }
+
+    const fullUrl = this.createUrl(
+      `api/repo/${options.uuid}/${options.endpoint}`
+    );
+    const { data } = await axios.get(fullUrl);
+    return data;
+  }
+
+  async createRepo(options) {
+    options = options || {};
+
+    if (!Object.hasOwnProperty.call(options, 'alias')) {
+      throw new Error('alias required to create data repository');
+    }
+    if (!Object.hasOwnProperty.call(options, 'description')) {
+      throw new Error('description required to create data repository');
+    }
+
+    const fullUrl = this.createUrl("api/repos");
+    await axios.post(fullUrl, options);
+  }
+
+  async node(options) {
+    options = options || {};
+    if (!Object.hasOwnProperty.call(options, 'uuid')) {
+      throw new Error('UUID required to access node data');
+    }
+    if (!Object.hasOwnProperty.call(options, 'endpoint')) {
+      throw new Error('endpoint required to access node data');
+    }
+
+    const fullUrl = this.createUrl(
+      `api/node/${options.uuid}/${options.endpoint}`
+    );
+    const { data } = await axios.get(fullUrl);
+    return data;
+  }
+}
+module.exports = DVID;
---
+++ src/index.test.js
@@ -0,0 +1,201 @@
+const axios = require("axios");
+const DVID = require("./index.js");
+const packageData = require("../package.json");
+const repoInfoData = require("../test/repo_info.json");
+const allReposInfo = require("../test/all_repos.json");
+
+const api = new DVID({
+  host: "https://emdata.janelia.org",
+  username: "dvidconsole",
+  application: "console-test",
+});
+
+const alternativeApi = new DVID({
+  host: "https://emdata.janelia.org",
+  port: "5678",
+  basename: "alt",
+  username: "dvidconsole",
+  application: "console-test",
+});
+
+const defaultAPI = new DVID();
+
+jest.mock("axios");
+
+it("reports it's own version", async () => {
+  const version = await api.version();
+  expect(version).toEqual(packageData.version);
+});
+
+it("generates the correct host strings", () => {
+  expect(api.hostString()).toEqual("https://emdata.janelia.org");
+  expect(alternativeApi.hostString()).toEqual("https://emdata.janelia.org:5678/alt/");
+  expect(defaultAPI.hostString()).toEqual("/");
+});
+
+it("creates the correct urls", () => {
+  expect(api.createUrl("/api/repos/info")).toEqual(
+    "https://emdata.janelia.org/api/repos/info?u=dvidconsole&app=console-test"
+  );
+  expect(alternativeApi.createUrl("/api/repos/info")).toEqual(
+    "https://emdata.janelia.org:5678/alt/api/repos/info?u=dvidconsole&app=console-test"
+  );
+  expect(alternativeApi.createUrl("api/repos/info")).toEqual(
+    "https://emdata.janelia.org:5678/alt/api/repos/info?u=dvidconsole&app=console-test"
+  );
+  expect(defaultAPI.createUrl("api/repos/info")).toEqual(
+    "/api/repos/info?u=anonymous&app=dvidjs"
+  );
+  expect(defaultAPI.createUrl("/api/repos/info")).toEqual(
+    "/api/repos/info?u=anonymous&app=dvidjs"
+  );
+});
+
+const serverInfo = {
+  Cores: "256",
+  "DVID Version": "v0.9.9",
+  "Datastore Version": "0.10.0",
+  Host: "59524cf95fe3:8000",
+  "Maximum Cores": "256",
+  Mode: "read only",
+  Note: "",
+  "RPC Address": ":8001",
+  "Server time": "2022-07-20 13:28:51.394086687 +0000 UTC m=+3058089.496712697",
+  "Server uptime": "849h28m9.482235322s",
+  "Storage backend":
+    "filestore; gbucket; ngprecomputed; swift; badger; basholeveldb; filelog",
+};
+
+it("gets serverInfo", async () => {
+  axios.get.mockResolvedValue({ data: serverInfo });
+  const info = await api.serverInfo();
+  expect(info).toEqual(serverInfo);
+});
+
+const serverTypes = {
+  annotation: "github.com/janelia-flyem/dvid/datatype/annotation",
+  keyvalue: "github.com/janelia-flyem/dvid/datatype/keyvalue",
+  labelmap: "github.com/janelia-flyem/dvid/datatype/labelmap",
+};
+
+it("gets serverTypes", async () => {
+  axios.get.mockResolvedValue({ data: serverTypes });
+  const info = await api.serverTypes();
+  expect(info).toEqual(serverTypes);
+});
+
+const serverCompiledTypes = {
+  annotation: "github.com/janelia-flyem/dvid/datatype/annotation",
+  float32blk: "github.com/janelia-flyem/dvid/datatype/imageblk/float32.go",
+  googlevoxels: "github.com/janelia-flyem/dvid/datatype/googlevoxels",
+  imagetile: "github.com/janelia-flyem/dvid/datatype/imagetile",
+  keyvalue: "github.com/janelia-flyem/dvid/datatype/keyvalue",
+  labelarray: "github.com/janelia-flyem/dvid/datatype/labelarray",
+  labelblk: "github.com/janelia-flyem/dvid/datatype/labelblk",
+  labelgraph: "github.com/janelia-flyem/dvid/datatype/labelgraph",
+  labelmap: "github.com/janelia-flyem/dvid/datatype/labelmap",
+  labelsz: "github.com/janelia-flyem/dvid/datatype/labelsz",
+  labelvol: "github.com/janelia-flyem/dvid/datatype/labelvol",
+  multichan16: "github.com/janelia-flyem/dvid/datatype/multichan16",
+  rgba8blk: "github.com/janelia-flyem/dvid/datatype/imageblk/rgba8.go",
+  roi: "github.com/janelia-flyem/dvid/datatype/roi",
+  tarsupervoxels: "github.com/janelia-flyem/dvid/datatype/tarsupervoxels",
+  uint16blk: "github.com/janelia-flyem/dvid/datatype/imageblk/uint16.go",
+  uint32blk: "github.com/janelia-flyem/dvid/datatype/imageblk/uint32.go",
+  uint64blk: "github.com/janelia-flyem/dvid/datatype/imageblk/uint64.go",
+  uint8blk: "github.com/janelia-flyem/dvid/datatype/imageblk/uint8.go",
+};
+
+it("gets serverCompiledTypes", async () => {
+  axios.get.mockResolvedValue({ data: serverCompiledTypes });
+  const info = await api.serverCompiledTypes();
+  expect(info).toEqual(serverCompiledTypes);
+});
+
+const load = {
+  "GET requests": 0,
+  "PUT requests": 0,
+  "active CGo routines": 0,
+  "file bytes read": 0,
+  "file bytes written": 0,
+  goroutines: 22,
+  "handlers active": 0,
+  "key bytes read": 0,
+  "key bytes written": 0,
+  "pending log messages": 0,
+  "value bytes read": 0,
+  "value bytes written": 0,
+};
+
+it("gets load", async () => {
+  axios.get.mockResolvedValue({ data: load });
+  const info = await api.load();
+  expect(info).toEqual(load);
+});
+
+
+
+it("gets reposInfo", async () => {
+  axios.get.mockResolvedValue({ data: allReposInfo });
+  const info = await api.reposInfo();
+  expect(info).toEqual(allReposInfo);
+});
+
+
+it("gets data from repo endpoint 'info'", async () => {
+  axios.get.mockResolvedValue({ data: repoInfoData });
+  const info = await api.repo({
+    uuid: "2b6d75f1c6e348ce945e1a66eb2f6bf3",
+    endpoint: "info",
+  });
+  expect(info).toEqual(repoInfoData);
+});
+
+it("throws an error if repo endpoint or uuid is missing", async () => {
+  await expect(async () => {
+    await api.repo();
+  }).rejects.toThrowError("UUID required to access repo data");
+
+  await expect(async () => {
+    await api.repo({uuid: "123456678"});
+  }).rejects.toThrowError("endpoint required to access repo data");
+});
+
+it("throws an error if node endpoint or uuid is missing", async () => {
+  await expect(async () => {
+    await api.node();
+  }).rejects.toThrowError("UUID required to access node data");
+
+  await expect(async () => {
+    await api.node({uuid: "123456678"});
+  }).rejects.toThrowError("endpoint required to access node data");
+});
+
+const nodeData = {
+  Base: {
+    Checksum: "No checksum",
+    Compression: "LZ4 compression, level -1",
+    DataUUID: "3de9db2953404bedb909f922fe53be42",
+    KVStore: "basholeveldb @ /data1/dbs/public-data/db-fib25",
+    LogStore: "write logs @ /data1/dbs/public-data/mutlog/fib25",
+    Name: "synapses",
+    RepoUUID: "2b6d75f1c6e348ce945e1a66eb2f6bf3",
+    Syncs: [],
+    Tags: {},
+    TypeName: "annotation",
+    TypeURL: "github.com/janelia-flyem/dvid/datatype/annotation",
+    TypeVersion: "0.1",
+    Versioned: true,
+  },
+  Extended: {},
+};
+
+it("gets data from node endpoint 'synapses/info'", async () => {
+  axios.get.mockResolvedValue({ data:nodeData });
+  const info = await api.node({
+    uuid: "2b6d75f1c6e348ce945e1a66eb2f6bf3",
+    endpoint: "synapses/info",
+  });
+  expect(info).toEqual(nodeData);
+});
+
